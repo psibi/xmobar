47b66bf4d8c411155320774d04aeb7a17d646a74
Sibi Prabakaran <sibi@psibi.in>
2020-06-01 10:11:14 +0530

    Add debug

--- src/Xmobar/App/Main.hs
+++ src/Xmobar/App/Main.hs
@@ -45,6 +45,7 @@ import Xmobar.App.EventLoop (startLoop, startCommand, newRefreshLock, refreshLoc
 import Xmobar.App.Compile (recompile, trace)
 import Xmobar.App.Config
 import Xmobar.App.Timer (withTimer)
+import System.IO
 
 xmobar :: Config -> IO ()
 xmobar conf = withDeferSignals $ do
@@ -56,6 +57,7 @@ xmobar conf = withDeferSignals $ do
                 (splitTemplate (alignSep conf) (template conf))
   sig   <- setupSignalHandler
   refLock <- newRefreshLock
+  hPutStrLn stderr (show cls)
   withTimer (refreshLock refLock) $
     bracket (mapM (mapM $ startCommand sig) cls)
             cleanupThreads
